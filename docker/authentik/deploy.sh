43afdd1fac972cfc4774474cc2cac6f7314e7b74
Harshit Mani Tripathi <harshit@hudater.dev>
2026-05-25 12:07:43 +0530

    trying authentik again; add: hosts to dozzle; fix: keycloak health check and up order

--- docker/authentik/deploy.sh
+++ docker/authentik/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CREATE_DIRS=( "${BAK_CFG_DIR}"/authentik/{authentik_server-media,authentik_server_custom-templates,authentik_worker-media,authentik_worker-certs,authentik_worker_custom-templates} "$DB_CFG_DIR"/authentik_redis "$DB_CFG_DIR"/authentik_postgres )
+CREATE_DIRS=( "${BAK_CFG_DIR}"/authentik/{authentik-worker-data,authentik-server-data,authentik_server-media,authentik_server_custom-templates,authentik_worker-media,authentik_worker-certs,authentik_worker_custom-templates} )
 mkdir -p "${CREATE_DIRS[@]}"
 # ls -alh "$BAK_CFG_DIR"/authentik/ "$DB_CFG_DIR"
 # ls -alh "${CREATE_DIRS[@]}"
--- docker/wgeasy/deploy.sh
+++ docker/wgeasy/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p "${BAK_CFG_DIR}"/wgeasy
-docker compose up -d
+docker compose up -d --force-recreate
